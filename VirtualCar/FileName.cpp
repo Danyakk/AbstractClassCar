bee334f1747dbdbb5b3885726cc078b2fb3f1095
Danyakk <kuhar1357@gmail,com>
2025-12-01 10:19:16 +0200

    1

--- VirtualCar/FileName.cpp
+++ VirtualCar/FileName.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <string>;
+#include <string>
 using namespace std;
 
 class Car {
@@ -18,7 +18,7 @@ public:
 		price = p;
 
 	}
-	void Print()
+	virtual void Print()
 	{
 		cout << "Material: " << material << "Color:" << color << "Weight: " << weight << "Price:" << price << endl;
 	}
@@ -29,15 +29,6 @@ public:
 	string GetMaterial()
 	{
 		return material;
-	}
-
-	void SetColor(string c)
-	{
-		color = c;
-	}
-	string GetColor()
-	{
-		return color;
 	}
 
 	void SetWeight(double w)
@@ -65,33 +56,153 @@ public:
 
 class SportCar : public Car {
 protected:
-	double speed;
 	string brand;
 	string model;
 	string engine;
 	double horsepower;
+	double speed;
 public:
-	SportCar() { weight = 0; price = 0; }
-	SportCar(double s, string b, string md, double e, double hp, string m, string c, double w, double p) : Car(m, c, w, p)
+	SportCar() { speed = 0.0; horsepower = 0; }
+	SportCar(string b, string md, string e, double hp, string m, string c, double w, double p) : Car(m, c, w, p)
 	{
-		speed = s;
 		brand = b;
 		model = md;
+		engine = e;
 		horsepower = hp;
 
 	}
-	void Print()
+
+	void SetBrand(string b)
 	{
-		cout << "speed: " << speed << "brand:" << brand << "model: " << model << "engine:" << engine << "horse power: " << horsepower << endl;
+		brand = b;
+	}
+	string GetBrand()
+	{
+		return brand;
 	}
 
+	void SetModel(string md)
+	{
+		model = md;
+	}
+	string GetModel()
+	{
+		return model;
+	}
 
+	void SetEngine(string e)
+	{
+		engine = e;
+	}
+	string GetEngine()
+	{
+		return engine;
+	}
+
+	void SetHorsePower(double hp)
+	{
+		horsepower = hp;
+	}
+	double GetHorsePower()
+	{
+		return horsepower;
+	}
+	string GetColor() const 
+	{
+		return color;
+	}
+	double GetSpeed() const 
+	{
+		return speed;
+	}
+	void Print()
+	{
+		Car::Print();
+		cout <<"brand:" << brand << "\tmodel: " << model << "\tengine:" << engine << "\thorse power: " << horsepower << endl;
+	}
 };
 
+class RetroCar : public Car {
+protected:
+	string year;
+	string country;
+	string type;
+	double speed;
+public:
+	RetroCar() { speed = 0.0; }
+	RetroCar(string y, string ct, string t, string m, string c, double w, double p) : Car(m, c, w, p)
+	{
+		year = y;
+		country = ct;
+		type = t;
+	}
+
+	void SetYear(string y)
+	{
+		year = y;
+	}
+	string GetYear()
+	{
+		return year;
+	}
+
+	void SetCountry(string ct)
+	{
+		country = ct;
+	}
+	string GetCountry()
+	{
+		return country;
+	}
+
+	void SetType(string t)
+	{
+		type = t;
+	}
+	string GetType()
+	{
+		return type;
+	}
+	virtual string GetColor() const
+	{
+		return color;
+	}
+	virtual double GetSpeed() const
+	{
+		return speed;
+	}
+	void Print()
+	{
+		Car::Print();
+		cout << "year:" << year << "\tcountry: " << country << "\ttype:" << type << endl;
+	}
+};
 
 
 
 int main()
 {
+	Car* car = nullptr;
+
+	int choice = 0;
+
+	cout << "1. Sport car" << endl;
+	cout << "2. Retro car" << endl;
+	cout << "Make your choice: ";
+
+	cin >> choice;
+
+	switch (choice)
+	{
+	case 1:
+		car = new SportCar("Ferrari", "488 Spider", "V8", 660, "Aluminum", "Red", 1475, 280000);
+		break;
+	case 2:
+		car = new RetroCar("1967", "USA", "Muscle Car", "Steel", "Blue", 1600, 35000, "120");
+		break;
+	}
+
+	car->Print();
+	cout << "Color: " << car->GetColor() << ", Speed: " << car->GetSpeed() << endl;
 
 }
